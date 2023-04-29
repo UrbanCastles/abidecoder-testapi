266b8cd54583cf7c0de9ddc5cbcb6c2fe6ef4b61
Reymark Urbano <bluern03@gmail.com>
2023-04-29 12:54:27 +0800

    Added routes and functionalities for decoding

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const http = require('http');
-const PORT = 3000;
+const express = require("express");
+const bodyParser = require('body-parser');
+const abiFunctions = require('./abiFunctions.js');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/POSTDecodeInputData', (req, res) => {
+    var decodedVal = abiFunctions.DecodeInputValues(req.body);
+    if(decodedVal.startsWith("ERROR"))
+    {
+        res.status(403).send(decodedVal);
+    }
+    else
+    {
+        res.status(200).send(decodedVal);
+    }
+  });
+
+app.get('/GETDecodeInputData', (req, res) => {
+    var decodedVal = abiFunctions.DecodeInputValues(req.query);
+    if(decodedVal.startsWith("ERROR"))
+    {
+        res.status(403).send(decodedVal);
+    }
+    else
+    {
+        res.status(200).send(decodedVal);
+    }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(PORT, () =>
+{
+    console.log("Listening on port " + PORT)
 });
+
+
+
