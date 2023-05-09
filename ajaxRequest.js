8d458473fa06db631bb9a34e1ba7935121a302e5
Reymark Urbano <bluern03@gmail.com>
2023-05-09 17:00:41 +0800

    Update ajaxRequest.js

--- ajaxRequest.js
+++ ajaxRequest.js
@@ -1,42 +1,24 @@
-var XMLHttpRequest = require('xhr2');
+const request = require('request');
 
 
-function AJAXCALLER_POST(URL, json_params, onSucessCallback, onErrorCallback = null) {
-
-    var AJAXSourceLinksMain = URL;
-    var data = EncodeJSONToURL(json_params);
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-    xmlhttp.open("POST", URL, true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            onSucessCallback(xmlhttp.responseText);
-        }
-
-        xmlhttp.onerror = function (e) {
-            onErrorCallback();
-        };
-
-    };
-    try {
-        xmlhttp.send(data);
-    }
-    catch (e) {
-        onErrorCallback();
-    }
-}
-
-function EncodeJSONToURL(jsonObj)
+function AJAXCALLER_POST(URL, json_params, onSucessCallback, onErrorCallback = null) 
 {
-    return Object.keys(jsonObj).map(function(k) {
-        return encodeURIComponent(k) + '=' + encodeURIComponent(jsonObj[k])
-    }).join('&');
+    request.post(URL, { json: json_params }, (error, response, body) => {
+      if (error) 
+      {
+          if(onErrorCallback !== null)
+          {
+              onErrorCallback();
+          }
+      } 
+      else 
+      {
+          if(onSucessCallback !== null)
+          {
+              onSucessCallback(body);
+          }
+      }
+    });
 }
 
 
